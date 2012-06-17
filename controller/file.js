aed84df1dfad484337cb828e2c6527d765a8dfe4
anizold <github@81008302.de>
2012-06-17 17:16:46 +0200

    files.jade

--- controller/file.js
+++ controller/file.js
@@ -4,9 +4,25 @@ var FileController = function(settings, resource, app, manager, utils) {
 		next();
 	};
 	
-	app.get('/files', prepare, function(req, res) {
+	app.get('/files', function(req, res) {
 		manager.file.get(req.query.path, req.query.cache, function(error, files) {
-			res.send(JSON.stringify(files));
+			
+			var folders = [];
+			var _files = [];
+			
+			for (var file in files[0]) {
+				if (files[file]._type === settings.dao.file.types.FILE) {
+					_files.push(files[file]);
+				} else if (files[file]._type === settings.dao.file.types.FOLDER) {
+					files[file].file = files[file].path.substring(files[file].path.lastIndexOf('/') + 1, files[file].path.length);
+					folders.push(files[file]);
+				}
+			}
+			
+			res.render('files', {
+				files: files.length > 0 ? files : null,
+				folders: folders.length > 0 ? folders : null
+			});
 		});
 	});
 	app.get('/files/index', prepare, function(req, res) {
