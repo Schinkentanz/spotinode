02d288534dcb1477a986212117d738b7667f24ac
tmarklstorfer <tobias.marklstorfer@googlemail.com>
2012-06-17 17:48:20 +0200

    click on die tds

--- public/js/script.js
+++ public/js/script.js
@@ -1,11 +1,22 @@
 $(document).ready(function() {
+	getFiles();
+
+	$('.songs').on('click', 'td' , function() {
+		getFiles($(this).data('path'));		
+	})
+});
+
+var getFiles = function(path) {
 	$.when($.ajax({
-		url: '/files',
-		type: 'get',
-		cache: false
+		url : '/files',
+		type : 'get',
+		cache : false,
+		data : {
+			path : path ? path : ''
+		}
 	})).then(function(html) {
 		$('.songs').append(html);
 	}, function() {
 		//error
 	});
-});
+};
