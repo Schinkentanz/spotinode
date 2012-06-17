66f5e3e2e1a73b441b426f9e62422f80b0c37dbf
anizold <git@81008302.de>
2012-06-17 15:31:51 +0200

    removed _old

--- _old/index.js
+++
@@ -1,9 +0,0 @@
-var resource = require('../resource.js').resource.index,
-	settings = require('../settings.js').settings,
-	fs = require('fs');
-
-exports.index = function(req, res) {
-	res.render('index', {
-		title: resource.title
-	})
-}; 
